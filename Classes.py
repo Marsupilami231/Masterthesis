dea1f692ec7f9019c5f38ac96990d5bd0d12859a
Marsupilami231 <116286104+Marsupilami231@users.noreply.github.com>
2022-10-31 14:36:38 +0100

    callback method added (no updating constraints yet)

--- Classes.py
+++ Classes.py
@@ -1,4 +1,6 @@
 import math
+import gurobipy as gp
+from gurobipy import GRB
 
 
 class Node:
@@ -37,5 +39,32 @@ class Truck:
 def calc_distance(start, end):
     dx = start[0] - end[0]
     dy = start[1] - end[1]
-    dist = math.sqrt(dx**2 + dy**2)
+    dist = math.sqrt(dx ** 2 + dy ** 2)
     return round(dist, 2)
+
+
+def callback(m, where):
+    if where == GRB.Callback.MIPSOL:
+        ind = {var[list(var.keys())[0]].VarName.split('[')[0]: {k: var[k].index for k in var} for var in
+               m._var_list}
+        vars = m.getVars()
+        sol = m.cbGetSolution(vars)
+        # m.addConstrs(
+        #     (t[j, k] >= t[i, k] + x[i, j, k] * d[i, j] / v[k] + s[j, k] * t_s[j] - Tw_max * (1 - x[i, j, k]) for i in
+        #      CvN for j
+        #      in V if (i, j) in A for k in K), name='time propagation')
+        for k in m._sets['K']:
+            for i in m._sets['CN']:
+                for j in m._sets['V']:
+                    if (i, j) in m._sets['A']:
+                        # check constraint for time propagation
+                        lhs = sol[ind['t'][j, k]]
+                        rhs = sol[ind['t'][i, k]] + sol[ind['x'][i, j, k]] * m._param['v'][k] * m._param['d'][
+                            i, j] + sol[ind['s'][j, k]] * m._param['t_s'][j] - m._param['Tw_max'] * (
+                                      1 - sol[ind['x'][i, j, k]])
+                        if lhs < rhs:
+                            m.cbLazy()
+                            print('Constraints not working)')
+
+    else:
+        pass
--- GurobiModel.py
+++ GurobiModel.py
@@ -5,60 +5,74 @@ import Classes
 import time
 from itertools import product
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 
 # import file
 file = 'Test_Daten.xlsx'
 wb = openpyxl.load_workbook(file)
 
 # definition of sets and parameters
-
+sets = dict()
+parameter = dict()
 # products
 W = wb['Products']['A']
 a = {W[i].value: wb['Products']['B'][i].value for i in range(1, len(W))}
 W = [W[i].value for i in range(1, len(W))]
+sets['W'] = W
+parameter['a'] = a
 # customers
 C = wb['Customers']['A']
 coord_cus = {C[i].value: [wb['Customers']['B'][i].value, wb['Customers']['C'][i].value] for i in range(1, len(C))}
-cus_y = {C[i].value: wb['Customers']['C'][i].value for i in range(1, len(C))}
 q = {C[i].value: {W[w]: float(wb['Customers']['D'][i].value.split(';')[w]) for w in range(len(W))} for i in
      range(1, len(C))}
 t_s = {C[i].value: wb['Customers']['E'][i].value for i in range(1, len(C))}
 C = [C[i].value for i in range(1, len(C))]
 
+parameter['q'] = q
+sets['C'] = C
+
 # auxiliary nodes
 N = wb['Nodes']['A']
 coord_nod = {N[i].value: [wb['Nodes']['B'][i].value, wb['Nodes']['C'][i].value] for i in range(1, len(N))}
-nod_y = {N[i].value: wb['Nodes']['C'][i].value for i in range(1, len(N))}
 N = [N[i].value for i in range(1, len(N))]
 t_s.update({i: 0 for i in N})
+sets['N'] = N
 
 # depots
 D = wb['Depots']['A']
 coord_dep = {D[i].value: [wb['Depots']['B'][i].value, wb['Depots']['C'][i].value] for i in range(1, len(D))}
-dep_y = {D[i].value: wb['Depots']['C'][i].value for i in range(1, len(D))}
 D = [D[i].value for i in range(1, len(D))]
 t_s.update({i: 0 for i in D})
-
+sets['D'] = D
+parameter['t_s'] = t_s
 # all vertices combined
 V = C + N + D
-CvN = C + N
+CN = C + N
+sets['V'] = V
+sets['CN'] = CN
 
 # Trucks
 K = wb['Trucks']['A']  # ID
 v = {K[i].value: wb['Trucks']['B'][i].value for i in range(1, len(K))}  # velocity
-c_d = {K[i].value: wb['Trucks']['B'][i].value for i in range(1, len(K))}  # depreciation cost
-Q = {K[i].value: wb['Trucks']['B'][i].value for i in range(1, len(K))}  # capacity
-c_f = {K[i].value: wb['Trucks']['B'][i].value for i in range(1, len(K))}  # fuel cost
+c_d = {K[i].value: wb['Trucks']['C'][i].value for i in range(1, len(K))}  # depreciation cost
+Q = {K[i].value: wb['Trucks']['D'][i].value for i in range(1, len(K))}  # capacity
+c_f = {K[i].value: wb['Trucks']['E'][i].value for i in range(1, len(K))}  # fuel cost
 K = [K[i].value for i in range(1, len(K))]
+parameter['c_d'] = c_d
+parameter['Q'] = Q
+parameter['c_f'] = c_f
+parameter['v'] = v
+sets['K'] = K
 
 # sizes
 S = wb['Sizes']['A']
 S = [S[i].value for i in range(1, len(S))]  # platoon sizes
+sets['S'] = S
 
 # right now we take all arcs
-customers = list(product(CvN, CvN))
-D_leave = list(product(D, CvN))
-D_arrive = list(product(CvN, D))
+customers = list(product(CN, CN))
+D_leave = list(product(D, CN))
+D_arrive = list(product(CN, D))
 A = customers + D_arrive + D_leave
 coord = coord_cus.copy()
 coord.update(coord_nod)
@@ -66,6 +80,8 @@ coord.update(coord_dep)
 d = {(i, j): Classes.calc_distance(coord[i], coord[j]) for (i, j) in A if i != j}
 A = list(d.keys())
 del customers, D_arrive, D_leave
+sets['A'] = A
+parameter['d'] = d
 
 # definition of parameters
 K_max = wb['Others']['B'][0].value
@@ -75,21 +91,32 @@ n_f = wb['Others']['B'][3].value  # fuel saving factor
 n_d = wb['Others']['B'][4].value  # driving relief factor
 c_l = wb['Others']['B'][5].value  # labor cost
 
+parameter['K_max'] = K_max
+parameter['Tw_max'] = Tw_max
+parameter['Td_max'] = Td_max
+parameter['n_f'] = n_f
+parameter['n_d'] = n_d
+parameter['c_l'] = c_l
+
+demand = {w: sum([q[i][w] for i in C]) for w in W}
+total_demand = sum([demand[w] * a[w] for w in W])
 # definition of variables
 m = gp.Model('VRP_TPMDSD')
 
-x = m.addVars(A, K, vtype=GRB.BINARY)
-y = m.addVars(C, K, W, vtype=GRB.CONTINUOUS, lb=0)
-t = m.addVars(V, K, vtype=GRB.CONTINUOUS, lb=0)
-tw = m.addVars(D, K, vtype=GRB.CONTINUOUS, lb=0)
+x = m.addVars(A, K, vtype=GRB.BINARY, name='x')
+y = m.addVars(C, K, W, vtype=GRB.CONTINUOUS, lb=0, name='y')
+t = m.addVars(V, K, vtype=GRB.CONTINUOUS, lb=0, name='t')
+tw = m.addVars(D, K, vtype=GRB.CONTINUOUS, lb=0, name='tw')
 # u = m.addVars(V, K, vtype=GRB.CONTINUOUS, lb=0)
-z = m.addVars(K, vtype=GRB.BINARY)
-s = m.addVars(V, K, vtype=GRB.BINARY)
-p = m.addVars(A, K, K, S, vtype=GRB.BINARY)
-pl = m.addVars(A, K, S, vtype=GRB.BINARY)
-pf = m.addVars(A, K, vtype=GRB.BINARY)
-ps = m.addVars(A, S, vtype=GRB.CONTINUOUS, lb=0)
-o = m.addVars(A, K, S, vtype=GRB.BINARY)
+z = m.addVars(K, vtype=GRB.BINARY, name='z')
+s = m.addVars(V, K, vtype=GRB.BINARY, name='s')
+p = m.addVars(A, K, K, S, vtype=GRB.BINARY, name='p')
+pl = m.addVars(A, K, S, vtype=GRB.BINARY, name='pl')
+pf = m.addVars(A, K, vtype=GRB.BINARY, name='pf')
+ps = m.addVars(A, S, vtype=GRB.CONTINUOUS, lb=0, name='ps')
+o = m.addVars(A, K, S, vtype=GRB.BINARY, name='o')
+
+var_list = [x, y, t, tw, z, s, p, pl, pf, ps, o]
 
 # subject to constraints
 # m.addConstrs((gp.quicksum(x[i, j, k] for i in V if (i, j) in A for k in K) >= 1 for j in C))
@@ -101,10 +128,10 @@ m.addConstrs(
 m.addConstrs((gp.quicksum(y[i, k, w] for w in W) <= Q[k] * s[i, k] for i in C for k in K), name='customer serving')
 m.addConstrs((y[i, k, w] <= Q[k] * gp.quicksum(x[i, j, k] for j in V if (i, j) in A) for k in K for i in C for w in W),
              name='only serve when visit')
-m.addConstrs((gp.quicksum(x[i, j, k] for j in V if (i, j) in A) <= Q[k] * z[k] for i in D for k in K))
-m.addConstrs(
-    (t[j, k] >= t[i, k] + x[i, j, k] * d[i, j] / v[k] + s[j, k] * t_s[j] - Tw_max * (1 - x[i, j, k]) for i in CvN for j
-     in V if (i, j) in A for k in K), name='time propagation')
+m.addConstrs((gp.quicksum(y[i, k, w] for i in C for w in W) <= Q[k] * z[k] for k in K))
+# m.addConstrs(
+#     (t[j, k] >= t[i, k] + x[i, j, k] * d[i, j] / v[k] + s[j, k] * t_s[j] - Tw_max * (1 - x[i, j, k]) for i in CN for j
+#      in V if (i, j) in A for k in K), name='time propagation')
 m.addConstrs(
     (t[j, k] >= tw[i, k] + x[i, j, k] * d[i, j] / v[k] + s[j, k] * t_s[j] - Tw_max * (1 - x[i, j, k]) for i in D for j
      in V if (i, j) in A for k in K), name='time propagation depot')
@@ -119,36 +146,37 @@ m.addConstrs((gp.quicksum(d[i, j] / v[k] * (x[i, j, k]) for (i, j) in A) <= Td_m
              name='max driving time')
 m.addConstrs((t[i, k] - tw[i, k] <= Tw_max for i in D for k in K), name='max working time')
 m.addConstrs((gp.quicksum(x[i, j, k] for j in V if (i, j) in A) <= 1 for k in K for i in C),
-             name='only one visit per node')
-m.addConstrs((gp.quicksum(x[i, j, k] for i in D for j in V if (i, j) in A) <= 1 for k in K), name='only one depot')
+             name='only one visit per customer')
+m.addConstrs((gp.quicksum(x[i, j, k] for i in D for j in V if (i, j) in A) == z[k] for k in K),
+             name='exactly one depot')
 
 # --- platoon formation ---
 
-m.addConstrs(
-    (x[i, j, k] + x[i, j, l] >= 2 * gp.quicksum(p[i, j, k, l, s] for s in S) for (i, j) in A for k in K for l in K if
-     l != k), name='platoon formation')
-m.addConstrs(
-    (t[i, k] - t[i, l] <= Tw_max * (1 - gp.quicksum(p[i, j, k, l, s] for s in S)) for i in CvN for j in V if (i, j) in A
-     for k in K for l in K if k != l), name='start time of platoon')
-m.addConstrs(
-    (tw[i, k] - tw[i, l] <= Tw_max * (1 - gp.quicksum(p[i, j, k, l, s] for s in S)) for i in D for j in V if (i, j) in A
-     for k in K for l in K if k != l), name='start time of platoon')
-m.addConstrs((p[i, j, k, l, s] == p[i, j, l, k, s] for (i, j) in A for k in K for l in K if k != l for s in
-              S), name='pairwise platoons')
-m.addConstrs(
-    ((s - 1) * o[i, j, k, s] == gp.quicksum(p[i, j, k, l, s] for l in K if k != l) for (i, j) in A for k in K for s in
-     S), name='size of platoon')
-m.addConstrs((gp.quicksum(o[i, j, k, s] for s in S) <= 1 for (i, j) in A for k in K), name='only one platoon per arc')
-m.addConstrs((s * ps[i, j, s] == gp.quicksum(o[i, j, k, s] for k in K) for (i, j) in A for s in S),
-             name='size of platoon')
-m.addConstrs((gp.quicksum(pl[i, j, k, s] for k in K) == ps[i, j, s] for (i, j) in A for s in S),
-             name='number platoon leader')
-m.addConstrs((pl[i, j, k, s] <= o[i, j, k, s] for (i, j) in A for k in K for s in S), name='platoon leader')
-m.addConstrs((pf[i, j, k] <= gp.quicksum(o[i, j, k, s] for s in S) for (i, j) in A for k in K), name='platoon follower')
-m.addConstrs((pf[i, j, k] + gp.quicksum(pl[i, j, k, s] for s in S) <= 1 for (i, j) in A for k in K),
-             name='position in platoon')
-m.addConstrs((tw[g, k] <= Tw_max * gp.quicksum(o[i, j, k, s] for (i, j) in A for s in S) for g in D for k in K),
-             name='wait time at depot')
+# m.addConstrs(
+#     (x[i, j, k] + x[i, j, l] >= 2 * gp.quicksum(p[i, j, k, l, s] for s in S) for (i, j) in A for k in K for l in K if
+#      l != k), name='platoon formation')
+# m.addConstrs(
+#     (t[i, k] - t[i, l] <= Tw_max * (1 - gp.quicksum(p[i, j, k, l, s] for s in S)) for i in CvN for j in V if (i, j) in A
+#      for k in K for l in K if k != l), name='start time of platoon')
+# m.addConstrs(
+#     (tw[i, k] - tw[i, l] <= Tw_max * (1 - gp.quicksum(p[i, j, k, l, s] for s in S)) for i in D for j in V if (i, j) in A
+#      for k in K for l in K if k != l), name='start time of platoon')
+# m.addConstrs((p[i, j, k, l, s] == p[i, j, l, k, s] for (i, j) in A for k in K for l in K if k != l for s in
+#               S), name='pairwise platoons')
+# m.addConstrs(
+#     ((s - 1) * o[i, j, k, s] == gp.quicksum(p[i, j, k, l, s] for l in K if k != l) for (i, j) in A for k in K for s in
+#      S), name='size of platoon')
+# m.addConstrs((gp.quicksum(o[i, j, k, s] for s in S) <= 1 for (i, j) in A for k in K), name='only one platoon per arc')
+# m.addConstrs((s * ps[i, j, s] == gp.quicksum(o[i, j, k, s] for k in K) for (i, j) in A for s in S),
+#              name='size of platoon')
+# m.addConstrs((gp.quicksum(pl[i, j, k, s] for k in K) == ps[i, j, s] for (i, j) in A for s in S),
+#              name='number platoon leader')
+# m.addConstrs((pl[i, j, k, s] <= o[i, j, k, s] for (i, j) in A for k in K for s in S), name='platoon leader')
+# m.addConstrs((pf[i, j, k] <= gp.quicksum(o[i, j, k, s] for s in S) for (i, j) in A for k in K), name='platoon follower')
+# m.addConstrs((pf[i, j, k] + gp.quicksum(pl[i, j, k, s] for s in S) <= 1 for (i, j) in A for k in K),
+#              name='position in platoon')
+# m.addConstrs((tw[g, k] <= Tw_max * gp.quicksum(o[i, j, k, s] for (i, j) in A for s in S) for g in D for k in K),
+#              name='wait time at depot')
 
 # objective function
 # fuel_cost = gp.quicksum(c_f[k] * d[i, j] * (x[i, j, k] - n_f * pf[i, j, k]) for (i, j) in A for k in K)
@@ -158,10 +186,48 @@ depreciation_cost = gp.quicksum(c_d[k] * z[k] for k in K)
 m.setObjective(fuel_cost + labor_cost + depreciation_cost, GRB.MINIMIZE)
 
 m.Params.MIPGap = 0.05
-m.optimize()
+m._var_list = var_list
+m._sets = sets
+m._param = parameter
+m.optimize(Classes.callback)
 
 edges = {key: x[key].x for key in x if x[key].x > 0}
 delivered = {key: y[key].x for key in y if y[key].x > 0}
+trucks = [key for key in z if z[key].x > 0]
+
+print(fuel_cost.getValue())
+print(labor_cost.getValue())
+print(depreciation_cost.getValue())
+
+for k in K:
+    for (i, j) in A:
+        if x[i, j, k].x > 0:
+            print((i, j, k))
+
+# print routes
+edges_truck = {k: dict() for k in K}
+for (i, j, k) in edges.keys():
+    edges_truck[k].update({i: j})
+
+routes = dict()
+for k, i in edges_truck.items():
+    if len(i) > 0:
+        s = list(set(D).intersection(list(i.keys())))
+        if len(s) > 0:
+            s = s[0]
+        else:
+            s = list(i.keys())[0]
+        route = [s, i[s]]
+        while i[s] != route[0]:
+            s = i[s]
+            route.append(i[s])
+        routes[k] = route
+
+for k, v in routes.items():
+    print(f'Route {k}: {v}')
+
+print(delivered)
+print(trucks)
 
 for i in D:
     plt.scatter(coord[i][0], coord[i][1], c='b')
@@ -172,15 +238,9 @@ for i in C:
 for i in N:
     plt.scatter(coord[i][0], coord[i][1], c='r')
 
+colors = {K[k]: list(mcolors.TABLEAU_COLORS.values())[k] for k in range(len(K))}
+
 for (i, j, k) in edges:
-    plt.plot([coord[i][0], coord[j][0]], [coord[i][1], coord[j][1]])
+    plt.plot([coord[i][0], coord[j][0]], [coord[i][1], coord[j][1]], colors[k])
 
 plt.show()
-
-print(d)
-print(fuel_cost.getValue())
-print(labor_cost.getValue())
-print(depreciation_cost.getValue())
-
-print(edges)
-print(delivered)
